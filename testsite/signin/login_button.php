0fbf5466aeb52d01a714536f9dd4592e38142e36
Vinson Lai <vinsonlai@uws107341usr.uws.ualberta.ca>
2016-03-27 21:24:29 -0600

    vinsons change

--- testsite/signin/login_button.php
+++ testsite/signin/login_button.php
@@ -9,11 +9,6 @@ $password=$_POST['Password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $connection = connect();
 print("connected");
-// To protect MySQL injection for Security purpose
-$username = stripslashes($username);
-$password = stripslashes($password);
-$username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
 // SQL query to fetch information of registerd users and finds user match.
 $query = oci_parse($connection,"select * from users where password='$password' AND username='$username'");
 oci_execute($query);
