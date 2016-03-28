4dc5b0d330ebd9bc33d0c4607d1b6fd1f3606d0b
Vinson Lai <vinsonlai@uws107341usr.uws.ualberta.ca>
2016-03-27 21:42:41 -0600

    vinson's changes

--- testsite/signin/login_button.php
+++ testsite/signin/login_button.php
@@ -8,7 +8,6 @@ $username=$_POST['User Name'];
 $password=$_POST['Password'];
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $connection = connect();
-print("connected");
 // SQL query to fetch information of registerd users and finds user match.
 $query = oci_parse($connection,"select * from users where password='$password' AND user_name='$username'");
 oci_execute($query);
@@ -18,7 +17,7 @@ if ($rows == 1) {
 $_SESSION['login_user']=$username; // Initializing Session
 header("location: login_button.php"); // Redirecting To Other Page
 } else {
-$error = "Username or Password is invalid";
+header("location: login_button.php");
 }
 }
 ?>
