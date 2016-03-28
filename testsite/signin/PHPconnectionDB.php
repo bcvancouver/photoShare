dec52ad0e1584659896a24b6e0547097b88d8e1e
Vinson Lai <vinsonlai@uws107341usr.uws.ualberta.ca>
2016-03-27 19:58:31 -0600

    connection??

--- testsite/signin/PHPconnectionDB.php
+++ testsite/signin/PHPconnectionDB.php
@@ -4,8 +4,9 @@ function connect(){
 	$conn = oci_connect('xi', 'Navyseal123');
 	if (!$conn) {
 		$e = oci_error();
-		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+		trigger_error(htmlentities($e['connect connect Oracle DB'], ENT_QUOTES), E_USER_ERROR);
 	}
+    print "Connected to Oracle!"
 	return $conn;
 }
 ?>
