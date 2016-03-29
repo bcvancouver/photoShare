55567deaf324dee1224db715b3cf840d591533ca
Vinson Lai <vinsonlai@uws107341usr.uws.ualberta.ca>
2016-03-28 18:55:57 -0600

    vinson's changes

--- testsite/signin/login_button.php
+++ testsite/signin/login_button.php
@@ -18,7 +18,9 @@ while ($row=oci_fetch_array($query,OCI_BOTH)){$r= $row[0];}
        // if username and password already exist in the database, we grant access
 	    $result=FALSE;
 	    if ($r!='0'){ $result=TRUE;}
-	    if ($result){	header("location:http://consort.cs.ualberta.ca/~wankinvi/photoShare391/testsite/signin/main.html");
+	    if ($result){
+        $_SESSION['login_user']=$username; // Initializing Session  
+                header("location:http://consort.cs.ualberta.ca/~wankinvi/photoShare391/testsite/signin/main.html");
 	    }
         else {
         print("not working");
