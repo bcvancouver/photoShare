c18e161a02c8e5b892518d13487907e1185c2936
Vinson Lai <vinsonlai@uws106885usr.uws.ualberta.ca>
2016-03-29 16:37:34 -0600

    vinson's changes

--- testsite/signin/getimage.php
+++ testsite/signin/getimage.php
@@ -1,29 +1,15 @@
 <?php
 include('PHPconnectionDB.php');
 session_start(); // Starting Session
-  $id = $_GET['id'];
-  //$type = $_GET['type'];
-  $type = 'photo';
-  $conn = connect();
-  $query = "SELECT photo FROM images WHERE subject = 'admin'";
-  
-  $stid = oci_parse($conn, $query);
-  
-  //oci_bind_by_name($stid, ":id", $id);
-  
-  
-  oci_execute($stid);
-  
-  $row = oci_fetch_array($stid);
-  $type = strtoupper($type);
-  $result = $row[$type]->load();
-  
-  header("Content-type: image/jpeg");
-  echo $result;
-  
-  oci_close($conn);
-?>
-
-
-<!http://stackoverflow.com/questions/8499633/how-to-display-base64-images-in-html>
-<!http://stackoverflow.com/questions/7793009/how-to-retrieve-images-from-mysql-database-and-display-in-an-html-tag>
+$sql = "SELECT image FROM images WHERE subject = " . (int) $_GET['id'];
+$conn = connection();
+$stid = oci_parse($conn, $sql);
+oci_execute($stid);
+$row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS);
+if (!$row) {
+    header('Status: 404 Not Found');
+} else {
+    $img = $row['IMAGE']->load();
+    header("Content-type: image/jpeg");
+    print $img;
+}
