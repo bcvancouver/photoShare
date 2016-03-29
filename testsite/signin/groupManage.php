e80728a65bf4f6de531853b460a7832d19de86da
MRijlaar <mrijlaar@ualberta.ca>
2016-03-28 19:55:18 -0600

    made navi.php needs include and function call

--- testsite/signin/groupManage.php
+++ testsite/signin/groupManage.php
@@ -1,4 +1,5 @@
- <!DOCTYPE html>
+ <?php include("navi.php"); ?> 
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -36,35 +37,7 @@
 
   <body role="document">
       <!-- Fixed navbar -->
-      <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="main.html">ExclusivePic</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="signin.html">Signin</a></li>
-                <li><a href="logout.php">Signout</a></li>
-                <li><a href="registration.html">Signup</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-    <div class="container">
+      <?php insertNavi(); ?>
     <div class="container">
 
 	      <form name="registration" method="post" action="groupCreate.php">
