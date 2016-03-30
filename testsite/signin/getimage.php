145175453e5cb820f171163561adfd82fa23c0e8
Vinson Lai <vinsonlai@uws107685usr.uws.ualberta.ca>
2016-03-30 14:55:23 -0600

    merge

--- testsite/signin/getimage.php
+++ testsite/signin/getimage.php
@@ -2,7 +2,7 @@
 include('PHPconnectionDB.php');
 session_start(); // Starting Session
 $user_name=$_SESSION["login_user"];
-$sql = "SELECT thumbnail FROM images WHERE owner_name = '$user_name'";
+$sql = "SELECT photo FROM images WHERE owner_name = '$user_name'";
 $conn = connect();
 print ("1working");
 $stid = oci_parse($conn, $sql);
