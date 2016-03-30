481ccb393984fb892722d0d14d62daf95a2e643e
Vinson Lai <vinsonlai@uws107685usr.uws.ualberta.ca>
2016-03-30 15:18:08 -0600

    merge

--- testsite/signin/getimage.php
+++ testsite/signin/getimage.php
@@ -7,16 +7,28 @@ $conn = connect();
 print ("1working");
 $stid = oci_parse($conn, $sql);
 oci_execute($stid);
-$showrow = oci_fetch_array($stid);
-echo '<img src="data:image/jpeg;base64,'.base64_encode( $showrow['image'] ).'"/>';
-print ("2working");
-if(!$showrow){
-print("Error in oci_fetch_row");
-return;
-}else{
+$showrow = oci_fetch_array($stid, OCI_ASSOC);
 $image=$showrow['0']->load();
 header("Content-type: image/JPEG");
 print $image;
 print ("3working");
 }
+?>
+
+<?php
+
+	$myblobid = $_GET['id'];;
+	$myimgtype = $_GET['type'];
+	include("connection_database.php");
+	$conn=connect();
+	$query = "SELECT ".$myimgtype." FROM images WHERE photo_id= :MYBLOBID";
+	$stmt = oci_parse ($conn, $query);
+	oci_bind_by_name($stmt, ':MYBLOBID', $myblobid);
+	oci_execute($stmt);
+	$arr = oci_fetch_array($stmt, OCI_ASSOC);
+	$myimgtype = strtoupper($myimgtype);
+	$result = $arr[$myimgtype]->load();
+	header("Content-type: image/jpg");
+	echo $result;
+	oci_close($conn);
 ?>
