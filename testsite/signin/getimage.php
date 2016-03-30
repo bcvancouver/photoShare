7570c61e754f2a8b7cda536af31d217c7b22e4cb
Vinson Lai <vinsonlai@uws107685usr.uws.ualberta.ca>
2016-03-30 14:36:49 -0600

    merge

--- testsite/signin/getimage.php
+++ testsite/signin/getimage.php
@@ -4,15 +4,17 @@ session_start(); // Starting Session
 $user_name=$_SESSION["login_user"];
 $sql = "SELECT thumbnail FROM images WHERE subject = '$user_name'";
 $conn = connect();
+print ("1working");
 $stid = oci_parse($conn, $sql);
 oci_execute($stid);
 $showrow = oci_fetch_row($stid);
+print ("2working");
 if(!$showrow){
 return;
 }else{
 $image=$showrow['0']->load();
 header("Content-type: image/JPEG");
 print $image;
-print ("working");
+print ("3working");
 }
 ?>
