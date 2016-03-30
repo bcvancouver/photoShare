76b47c26c24a4808b2e45dc6de289887828cad9a
Vinson Lai <vinsonlai@uws107685usr.uws.ualberta.ca>
2016-03-30 17:18:49 -0600

    merge

--- testsite/signin/getimage.php
+++ testsite/signin/getimage.php
@@ -7,13 +7,15 @@ $conn = connect();
 $stid = oci_parse($conn, $sql);
 oci_execute($stid);
 $showrow = oci_fetch_array($stid,OCI_ASSOC+OCI_RETURN_NULLS);
+if (!$showrow) {  print('Status: 404 Not Found');
+    header('Status: 404 Not Found');
+    die();
+}
 while ($showrow) {
   $img = $showrow['PHOTO']->load();
     header("Content-type: image/jpeg");
     print $img;
-} else {
-    
-    print('Status: 404 Not Found');
-    header('Status: 404 Not Found');
 }
+    
+  
 ?>
