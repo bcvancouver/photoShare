40e3031e2529209553b96080396b6cfa83477d45
Vinson Lai <vinsonlai@uws107685usr.uws.ualberta.ca>
2016-03-30 17:24:20 -0600

    merge

--- testsite/signin/getimage.php
+++ testsite/signin/getimage.php
@@ -2,7 +2,7 @@
 include('PHPconnectionDB.php');
 session_start(); // Starting Session
 $user_name=$_SESSION["login_user"];
-$sql = "SELECT photo FROM images WHERE owner_name = '$user_name'";
+$sql = "SELECT thumbnail FROM images WHERE owner_name = '$user_name'";
 $conn = connect();
 $stid = oci_parse($conn, $sql);
 oci_execute($stid);
@@ -12,7 +12,7 @@ if (!$showrow) {  print('Status: 404 Not Found');
     die();
 }
 while ($showrow) {
-  $img = $showrow['PHOTO']->load();
+  $img = $showrow['thumbnail']->load();
     header("Content-type: image/jpeg");
     print $img;
 }
