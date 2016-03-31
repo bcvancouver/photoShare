6121150ec48ceff757c483d9682931c49f9fee28
Wan Kin Vinson Lai <wankinvi@cs.ualberta.ca>
2016-03-30 21:49:53 -0600

    hi

---
+++ testsite/signin/thumb.php
@@ -0,0 +1,27 @@
+
+	<?php 
+	include('PHPconnectionDB.php');
+
+session_start(); // Starting Session
+$user_name=$_SESSION["login_user"];
+$n = $_GET['n'];
+
+
+if (n == '1') {
+$sql = "SELECT photo_id FROM images";
+}
+if (n == '2') {
+$sql = "SELECT photo_id FROM images";
+}
+
+
+$conn = connect();
+$stid = oci_parse($conn, $sql);
+oci_execute($stid);
+while($showrow = oci_fetch_array($stid,OCI_ASSOC+OCI_RETURN_NULLS) ) {
+  		$id = $showrow['PHOTO_ID'];
+  		echo '<img src='getimage.php?id=$id' />;
+}
+
+
+?>
