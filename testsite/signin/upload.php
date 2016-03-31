ae3b19e315886ab3712778ab72cc58ebadeda811
bcvancouver <michael-xi@hotmail.com>
2016-03-30 22:19:47 -0600

    try randys function

--- testsite/signin/upload.php
+++ testsite/signin/upload.php
@@ -4,7 +4,7 @@
     session_start();
     $user=$_SESSION['login_user'];
     $connection=connect();
-    define('MAX_THUMBNAIL_DIMENSION',200);
+    define('MAX_THUMBNAIL_DIMENSION',100);
 
 
     /*
@@ -38,45 +38,58 @@
 
 
     //Function to turn picture into thumbnail
-    function getThumbnail($imgfile)
-    {
-        list($w, $h, $type) = getimagesize($imgfile);
-
-        // Retrieve old image
-        switch ($type) {
-            case IMAGETYPE_GIF:
-                $src_img = imagecreatefromgif($imgfile);
-                break;
-            case IMAGETYPE_JPEG:
-                $src_img = imagecreatefromjpeg($imgfile);
-                break;
-            default:
-                throw new Exception('Unrecognized image type ' . $type);
+    function getThumbnail($file) {
+        //$source_pic = $file;
+        $max_width = 200;
+        $max_height = 200;
+        echo "<center>processing!</center><br/>";
+        list($width, $height, $image_type) = getimagesize($file);
+        switch ($image_type)
+        {
+            case 1: $src = imagecreatefromgif($file); break;
+            case 2: $src = imagecreatefromjpeg($file);  break;
+            case 3: $src = imagecreatefrompng($file); break;
+            default: return '';  break;
         }
-
-        if ($w > MAX_THUMBNAIL_DIMENSION || $h > MAX_THUMBNAIL_DIMENSION) {
-            // Rescale image to thumbnail size
-            $scale = MAX_THUMBNAIL_DIMENSION / (($h > $w) ? $h : $w);
-            $nw = $w * $scale;
-            $nh = $h * $scale;
-            $dest_img = imagecreatetruecolor($nw, $nh);
-            imagecopyresampled($dest_img, $src_img, 0, 0, 0, 0, $nw, $nh, $w, $h);
-            // Create new thumbnail from old image
-            switch ($type) {
-                case IMAGETYPE_JPEG:
-                    // overwrite file with new thumbnail
-                    imagejpeg($dest_img, $imgfile);
-                    break;
-                case IMAGETYPE_GIF:
-                    imagegif($dest_img, $imgfile);
-                    break;
-                default:
-                    throw new Exception('Unrecognized image type ' . $type);
-            }
-            // Clean up
-            imagedestroy($src_img);
-            imagedestroy($dest_img);
+        $x_ratio = $max_width / $width;
+        $y_ratio = $max_height / $height;
+        if( ($width <= $max_width) && ($height <= $max_height) ){
+            $tn_width = $width;
+            $tn_height = $height;
+        }elseif (($x_ratio * $height) < $max_height){
+            $tn_height = ceil($x_ratio * $height);
+            $tn_width = $max_width;
+        }else{
+            $tn_width = ceil($y_ratio * $width);
+            $tn_height = $max_height;
         }
+        $tmp = imagecreatetruecolor($tn_width,$tn_height);
+        /* Check if this image is PNG or GIF, then set if Transparent*/
+        if(($image_type == 1) OR ($image_type==3))
+        {
+            imagealphablending($tmp, false);
+            imagesavealpha($tmp,true);
+            $transparent = imagecolorallocatealpha($tmp, 255, 255, 255, 127);
+            imagefilledrectangle($tmp, 0, 0, $tn_width, $tn_height, $transparent);
+        }
+        imagecopyresampled($tmp,$src,0,0,0,0,$tn_width, $tn_height,$width,$height);
+        /*
+         * imageXXX() only has two options, save as a file, or send to the browser.
+         * It does not provide you the oppurtunity to manipulate the final GIF/JPG/PNG file stream
+         * So I start the output buffering, use imageXXX() to output the data stream to the browser,
+         * get the contents of the stream, and use clean to silently discard the buffered contents.
+         */
+        ob_start();
+        switch ($image_type)
+        {
+            case 1: imagegif($tmp); break;
+            case 2: imagejpeg($tmp, NULL, 100);  break; // best quality
+            case 3: imagepng($tmp, NULL, 0); break; // no compression
+            default: echo ''; break;
+        }
+        $final_image = ob_get_contents();
+        ob_end_clean();
+        return $final_image;
     }
 
      //Check image file
