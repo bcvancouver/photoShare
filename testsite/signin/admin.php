1a54cddc67689ffc9689934367ff8d69b6a345d5
bcvancouver <michael-xi@hotmail.com>
2016-04-01 04:37:44 -0600

    testing

--- testsite/signin/admin.php
+++ testsite/signin/admin.php
@@ -220,7 +220,7 @@ function getOptions($sql) {
                         </li>
                         <li class="divider"></li>-->
                         <li>
-                            <a href="#">"signin.html"<i class="fa fa-fw fa-gear"></i> Log Out</a>
+                            <a href="signin.html"><i class="fa fa-fw fa-gear"></i>Log Out</a>
                         </li>
                     </ul>
                 </li>
