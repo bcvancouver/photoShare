afef52ff18f6c82307225705751a07e07a53c6cb
Vinson Lai <vinsonlai@Vinsonteki-MacBook-Pro.local>
2016-04-01 11:48:14 -0600

    at display

--- testsite/signin/thumb.php
+++ testsite/signin/thumb.php
@@ -26,12 +26,12 @@ if ($user_name == "admin" and $n == "admin") {
 	}
 	elseif ($n == "2") {
 		//oldest
-		$query = "SELECT photo_id FROM images order by timing desc";	
+		$query = "select t1.photo_id from (select photo_id from images where permitted = '1' or owner_name = '$user_name' or permitted in (select group_id from group_lists where friend_id = 'user_name' union select group_id from groups where user_name = '$user_name' ))t1 inner join (SELECT photo_id FROM images order by timing desc)t2 on t1.photo_id = t2.photo_id";	
 				//$query = "select photo_id from images where permitted = '1' or owner_name = '$user' or permitted in (select group_id from group_lists where friend_id = '$user' union select group_id from groups where user_name = '$user' )";
 	}
 	elseif ($n == "3") {
 		//top 5 popular images
-		$query = "select photo_id from (select photo_id, count(photo_id) as visits from photo_visit group by photo_id order by visits desc)where ROWNUM <=5";
+		$query = "select t1.photo_id from (select photo_id from images where permitted = '1' or owner_name = '$user_name' or permitted in (select group_id from group_lists where friend_id = 'user_name' union select group_id from groups where user_name = '$user_name' ))t1 inner join (select photo_id from (select photo_id, count(photo_id) as visits from photo_visit group by photo_id order by visits desc) where ROWNUM <=5))t2 on t1.photo_id = t2.photo_id";
 				//$query = "select photo_id from images where permitted = '1' or owner_name = '$user' or permitted in (select group_id from group_lists where friend_id = '$user' union select group_id from groups where user_name = '$user' )";
 	}
 	else {
