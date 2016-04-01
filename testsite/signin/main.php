234b34147d7b367c092956ea7b5730a0af1bded7
Vinson Lai <vinsonlai@uws107895usr.uws.ualberta.ca>
2016-03-31 21:23:15 -0600

    at display

--- testsite/signin/main.php
+++ testsite/signin/main.php
@@ -84,34 +84,7 @@
 
   <body role="document">
       <!-- Fixed navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="main.html">ExclusivePic</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="groupManage.php">Groups</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="signin.html">Sign in</a></li>
-                <li><a href="logout.php">Sign out</a></li>
-                <li><a href="registration.html">Sign up</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+    <?php include("navi.php"); ?> 
     <div class="container">
 
 
