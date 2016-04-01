8bc109cf8fc6ae2deed7c28a884414e11be29bb8
Vinson Lai <vinsonlai@uws107895usr.uws.ualberta.ca>
2016-03-31 19:01:07 -0600

    at display

--- testsite/signin/updateinfo.php
+++ testsite/signin/updateinfo.php
@@ -9,7 +9,6 @@
           $c = $_POST['address'];
           $d = $_POST['email'];
           $e = $_POST['phonenumber'];
-    print($a);
     
             $sql = 'update persons set first_name = \''.$a.'\', last_name = \''.$b.'\', address = \''.$c.'\', email = \''.$d.'\',phone  = \''.$e.'\' 
             where user_name = \''.$user.'\'';
@@ -21,28 +20,26 @@
     		$e = oci_error();
     		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 	    }
-			//echo "hello $user";
 			
-			$sql='select * from Persons where user_name=\''.$user.'\'';
-			//echo $sql;
-	    //Prepare sql using conn and returns the statement identifier
-	    $stid = oci_parse($conn, $sql);
-	    
-	    //Execute a statement returned from oci_parse()
-	    $res=oci_execute($stid);
-	    
-	    while ($row=oci_fetch_array($stid,OCI_BOTH)){
-	    	//echo "good";
-	    	$username= $row[0]; 
-	    	$firstname=$row[1];
-	    	$lastname=$row[2];
-	    	$address=$row[3];
-	    	$email=$row[4];
-	    	$phone=$row[5];
-	    	}
-	    //$row=oci_fetch_array($stid,OCI_BOTH)
+        execute_queries($conn,$sql);
 	    oci_free_statement($stid);
 	    oci_close($conn);			
+
+function execute_queries($conn, $sql)	{
+		 
+		 $stid = oci_parse($conn, $sql);
+		 echo "the conn is $conn ";
+	    echo "inserting $sql \n";
+	    //Execute a statement returned from oci_parse()
+	    $res=oci_execute($stid);
+	    if (!$res) {
+		 $err = oci_error($stid); 
+		 echo htmlentities($err['message']);
+		 
+	    }
+	    oci_free_statement($stid);
+	    oci_close($conn);
+}
 	    
 //header("location: main.php");// Redirecting back to log in page
         
