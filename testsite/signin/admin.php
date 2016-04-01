d0bb99b649ffe7138f33c8f299ae2e7e5a2707db
bcvancouver <michael-xi@hotmail.com>
2016-04-01 04:48:51 -0600

    testing

--- testsite/signin/admin.php
+++ testsite/signin/admin.php
@@ -134,7 +134,7 @@ function getOptions($sql) {
                         </li>
                         <li class="divider"></li>
                         <li>
-                            <a href="signin.html" onclick="<?php session_abort();?>"><i class="fa fa-fw fa-gear"></i>Log Out</a>
+                            <a href="signin.html" ><i class="fa fa-fw fa-gear"></i>Log Out</a>
                         </li>
                     </ul>
                 </li>
